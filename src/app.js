12bcd4adc56c21980ceba2ad2a62d7b0f5ff1d87
wanderer-s <ows0871@gmail.com>
2021-04-13 23:30:45 +0900

    index route 변수명 변경

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const app = express()
 
-const indexRouter = require('./routes/index')
+const routes = require('./routes')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
