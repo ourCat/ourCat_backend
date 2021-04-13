20f240eb9976e3756af4d3b8234484cfe2d61f5d
wanderer-s <ows0871@gmail.com>
2021-04-13 23:27:48 +0900

    swagger apis 에 src 추가

--- src/routes/swagger/swagger.js
+++ src/routes/swagger/swagger.js
@@ -4,33 +4,33 @@ const swaggerJSDoc = require('swagger-jsdoc')
 
 const swaggerDefinition = {
   openapi: '3.0.0',
-  info: { 
-      title: 'OURCAT',
-      version: '1.0.0',
-      description: '우리 고양이를 위한 api'
+  info: {
+    title: 'OURCAT',
+    version: '1.0.0',
+    description: '우리 고양이를 위한 api'
   },
   host: '/',
   servers: [
-      {url: '/'}
+    { url: '/' }
   ],
   securitySchemes: {
-      jwt: {
-          type: 'apiKey',
-          name: 'Authorization',
-          in: 'header'
-      }
+    jwt: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header'
+    }
   },
-  security: [ { jwt: [] } ]
-};
+  security: [{ jwt: [] }]
+}
 
 const options = {
   swaggerDefinition,
-  apis: [ 'routes/*.yml', 'routes/*/*.yml']
-};
-const swaggerSpec = swaggerJSDoc(options);
+  apis: [ 'src/routes/*.yml', 'src/routes/*/*.yml']
+}
+const swaggerSpec = swaggerJSDoc(options)
 
 router.use('/', swaggerUi.serveWithOptions({cacheControl: false}))
 router.get('/', swaggerUi.setup(swaggerSpec))
 
 
-module.exports = router;
+module.exports = router
