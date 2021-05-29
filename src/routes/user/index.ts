a42ed46f6f7ef4f221195b123793ea0fd85128a4
wanderer-s <ows0871@gmail.com>
2021-05-30 03:03:57 +0900

    Resolve ourCat#3 feature/signin
    - /user/signin api 추가
    - jwt 사용
    - swagger api문서 정리

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from 'express'
 const router = Router()
 import bcrypt from 'bcrypt'
 import asyncWrap from 'utils/asyncWrap'
+import jwt from 'jsonwebtoken'
 import createError from 'http-errors'
 
 import dayjs from 'dayjs'
@@ -17,7 +18,6 @@ router.get('/', asyncWrap(async (req: Request, res: Response) => {
   const users = await getUsers()
   res.status(200).json({users})
 }))
-
 
 router.post('/signup', asyncWrap(async (req: Request, res: Response) => {
   const { email, password, passwordConfirm, mobileNo, gender }:
@@ -61,4 +61,21 @@ router.post('/signup', asyncWrap(async (req: Request, res: Response) => {
   res.status(201).json({status: 'OK'})
 }))
 
+router.post('/signin', asyncWrap(async (req: Request, res: Response) => {
+  const {loginId, password}: {loginId: string, password: string} = req.body
+  if(!loginId || !password) throw createError(400, 'Required loginId or password')
+
+  const foundUser = await getUserByLoginId(loginId)
+  if(!foundUser) throw createError(400, 'Invalid loginId or password')
+
+  if(!await bcrypt.compare(password, foundUser.password)) throw createError(400, 'Invalid loginId or password')
+
+  const userToken = jwt.sign({
+    userId: foundUser._id,
+    iss: 'dev-Cats',
+    exp: Math.floor(Date.now() / 1000) + 3600 * 24 * 30
+  }, process.env.USER_TOKEN_KEY)
+
+  res.status(200).json({userToken})
+}))
 export default router
