0b5cce758d1323b8c83731f79a23727924e470b0
wanderer-s <ows0871@gmail.com>
2021-09-01 00:21:02 +0900

    resolves #21 feature/naver_login
    - /user/social/ switch문으로 Refactoring(반복코드 줄임)
    - naver login 추가

--- src/routes/user/social.ts
+++ src/routes/user/social.ts
@@ -14,28 +14,52 @@ dayjs.extend(timezone)
 import {signup, getUserByLoginId, updateLastAccessedAt} from 'db/user'
 
 
-router.post('/kakao', asyncWrap( async(req: Request, res: Response) => {
-  const { token } : {token: string} = req.body
-
+router.post('/:socialType', asyncWrap(async (req: Request, res: Response) => {
+  const { token }: { token: string } = req.body
+  const { socialType }: { socialType: string } = req.params
+  let user, userToken: string
   try {
-    const {data} = await axios('https://kapi.kakao.com/v2/user/me', {headers: {'Authorization': `Bearer ${token}`}})
-    const {gender, email} : {gender: string, email: string} = data.kakao_account
     const now = dayjs().toDate()
-    const user = {
-      socialId : data.id,
-      nickname: data.properties.nickname,
-      socialType: 'kakao',
-      email: email,
-      loginId : `kakao_${data.id}`,
-      gender : gender === 'male' ? 'M' : 'F',
-      lastAccessedAt: now,
-      createdAt: now,
-      updatedAt: now
+    switch (socialType) {
+      case 'kakao': {
+        const { data } = await axios('https://kapi.kakao.com/v2/user/me', { headers: { 'Authorization': `Bearer ${token}` } })
+        const { gender, email }: { gender: string, email: string } = data.kakao_account
+
+        user = {
+          socialId: data.id,
+          nickname: data.properties.nickname,
+          socialType: 'kakao',
+          email: email,
+          loginId: `kakao_${data.id}`,
+          gender: gender === 'male' ? 'M' : 'F',
+          lastAccessedAt: now,
+          createdAt: now,
+          updatedAt: now
+        }
+        break
+      }
+      case 'naver': {
+        const { data: { response } } = await axios('https://openapi.naver.com/v1/nid/me', { headers: { 'Authorization': `Bearer ${token}` } })
+        user = {
+          socialId: response.id,
+          nickname: response.nickname,
+          socialType: 'naver',
+          email: response.email,
+          loginId: `naver_${response.id}`,
+          gender: response.gender,
+          lastAccessedAt: now,
+          createdAt: now,
+          updatedAt: now
+        }
+        break
+      }
+
+      default:
+        console.log(socialType)
+        throw createError(404)
     }
 
-    
     const foundUser = await getUserByLoginId(user.loginId)
-    let userToken
     if (foundUser) {
       await updateLastAccessedAt(foundUser._id)
       userToken = jwt.sign({
@@ -44,16 +68,16 @@ router.post('/kakao', asyncWrap( async(req: Request, res: Response) => {
         exp: Math.floor(Date.now() / 1000) + 3600 * 24 * 30
       }, process.env.USER_TOKEN_KEY)
     } else {
-      const {insertedId} : {insertedId: string} = await signup(user)
+      const { insertedId }: { insertedId: string } = await signup(user)
       userToken = jwt.sign({
         userId: insertedId,
         iss: 'dev-Cats',
         exp: Math.floor(Date.now() / 1000) + 3600 * 24 * 30
       }, process.env.USER_TOKEN_KEY)
-
     }
-    res.status(200).json(userToken)
+    res.status(200).json({ userToken })
   } catch (error) {
+    if (error.message === 'Not Found') throw createError(404)
     throw createError(401, 'Invalid Token or expired')
   }
 }))
