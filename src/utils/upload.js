356078f8280c9d0a6b1102feaf57dd3dbe3568ba
wanderer-s <ows0871@gmail.com>
2021-10-17 17:39:49 +0900

    upload 수정

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,24 +1,15 @@
-const AWS = require('aws-sdk');
-
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
 
 const path = require('path')
 
-const { AWS_config_region, AWS_IDENTITYPOOLID } = process.env
-
 const bucket = 'ourcats'
 
-AWS.config.update({
-  region : AWS_config_region,
-  credentials : new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: AWS_IDENTITYPOOLID
-  })
-})
-
 const s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {Bucket: bucket}
+  accessKeyId: process.env.AWS_ACCESSKEY,
+  secretAccessKey: process.env.AWS_SECRETKEY,
+  region: process.env.AWS_config_region
 })
 
 const upload = multer({
@@ -27,6 +18,9 @@ const upload = multer({
     bucket: bucket,
     contentType: multerS3.AUTO_CONTENT_TYPE, // 자동으로 콘텐츠 타입 세팅
     acl: 'public-read',
+    metadata: (req, file, cb) => {
+      cb(null, {filedName: file.fieldname})
+    },
     key: (req, file, cb) => {
       const extension = path.extname(file.originalname)
       cb(null, Date.now().toString()+extension)
